d9ba0618089760d0f2569757c94319c531f019d6
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-11 20:49:59 +0800

    Add files via upload

---
+++ javacode/Main4.java
@@ -0,0 +1,12 @@
+public class Main4 {
+
+	public static void main(String[] args) {
+		int cash = 10_000;
+		System.out.println(cash);
+		System.out.printf("%,d%n", cash);
+		double pi = 3.1415926;
+		System.out.printf("%.3f\n", pi);
+		
+	}
+
+}
