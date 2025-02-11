9e9ed864de7327f91bd7cc63504f200583a248e9
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-11 19:49:14 +0800

    Add files via upload

---
+++ javacode/Main3.java
@@ -0,0 +1,14 @@
+public class Main3 {
+	public static void main(String[] args) {
+		System.out.println(5+2); // 7
+		System.out.println(5-2); // 3
+		System.out.println(5*2); // 10
+		System.out.println(5/2); // 2
+		System.out.println(5/2.0); // 2.5
+		System.out.println(5%2); // 1
+		System.out.println(2_147_483_647);
+		System.out.println(2_147_483_648L);
+		System.out.println(3.1415926);
+		System.out.println(3.1415926f);
+	}
+}
