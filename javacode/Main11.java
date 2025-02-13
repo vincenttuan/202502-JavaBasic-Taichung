3eb2ec130249a538669f630409aea9d76fb5989b
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-13 19:48:36 +0800

    Add files via upload

---
+++ javacode/Main11.java
@@ -0,0 +1,10 @@
+public class Main11 {
+	public static void main(String[] args) {
+		// boolean 包含 true 或 false
+		int score = 85; // 成績
+		boolean isPass = score >= 60;
+
+		System.out.printf("score=%d isPass=%b%n", score, isPass);
+
+	}
+}
