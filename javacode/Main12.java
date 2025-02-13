540c72aecf0bfcc810f3afeb03501ff3de280b3d
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-13 21:21:21 +0800

    Add files via upload

--- javacode/Main12.java
+++ javacode/Main12.java
@@ -1,15 +1,15 @@
 /*
 	飲料一瓶 10 元
-	買二送一
-	請問買 10 瓶要多少錢 ?
+	買二送三
+	請問買 11 瓶要多少錢 ?
 */
 public class Main12 {
 	public static void main(String[] args) {
 		int price = 10; // 飲料價格
-		int buy = 10;    // 要買的瓶數
+		int buy = 11;    // 要買的瓶數
 
-		int set = (buy/3)*2; // 有幾組
-		int mod = buy%3; // 餘數
+		int set = (buy/5)*2; // 有幾組
+		int mod = buy%5; // 餘數
 
 		int total = (set + mod) * price;
 
