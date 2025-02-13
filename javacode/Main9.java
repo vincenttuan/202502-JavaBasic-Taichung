24db25019e1ba3cd07a27dcc43a73e40d88fa0d9
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-13 20:43:38 +0800

    Add files via upload

--- javacode/Main9.java
+++ javacode/Main9.java
@@ -5,10 +5,12 @@ public class Main9 {
 		char h2 = '\u0048'; // 'H' 的 unicode 值
 		char c = '\u9527'; // 因 unicode 無此資料所以執行後會呈現 ?
 
+		char d = '\''; // \ 跳脫字元
+
 		System.out.printf("h1=%c%n", h1);
 		System.out.printf("h2=%c%n", h2);
 		System.out.printf("c=%c%n", c);
-
+		System.out.printf("d=%c%n", d);
 	}
 
 }
