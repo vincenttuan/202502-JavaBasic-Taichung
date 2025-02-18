1481e817ab236650232196997f809a0a8604158c
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-18 20:01:18 +0800

    Add files via upload

---
+++ javacode/Main19.java
@@ -0,0 +1,36 @@
+public class Main19 {
+	// 月份 12, 1, 2 冬
+	// 月份  3, 4, 5 春
+	// 月份  6, 7, 8 夏
+	// 月份  9, 10, 11 秋
+
+	public static void main(String[] args) {
+		int month = 2;
+
+		// 請印出季節
+		if(month == 12 || month == 1 || month == 2) {
+			System.out.println("冬");
+		}
+		if(month == 3 || month == 4 || month == 5) {
+			System.out.println("春");
+		}
+		if(month == 6 || month == 7 || month == 8) {
+			System.out.println("夏");
+		}
+		if(month == 9 || month == 10 || month == 11) {
+			System.out.println("秋");
+		}
+
+		// ---------------------------------------------
+		if(month == 12 || month == 1 || month == 2) {
+			System.out.println("冬");
+		} else if(month == 3 || month == 4 || month == 5) {
+			System.out.println("春");
+		} else if(month == 6 || month == 7 || month == 8) {
+			System.out.println("夏");
+		} else if(month == 9 || month == 10 || month == 11) {
+			System.out.println("秋");
+		}
+		
+	}
+}
